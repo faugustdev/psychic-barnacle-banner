0b4b2b31fdd9873f9390b17030093b45b305d12d
FranciscoAugust <francisco.august.fa@gmail.com>
2025-07-04 00:26:32 -0400

    Agregar script de ejemplo para ejecutar el banner remoto desde GitHub

---
+++ ejemplo_banner_remoto.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Ejemplo de uso: Ejecutar el banner remoto desde GitHub
+
+# Ejecuta el banner directamente desde el repositorio de GitHub
+bash <(curl -s https://raw.githubusercontent.com/faugustdev/psychic-barnacle-banner/main/banner_autor_v2.sh)
+
+echo "Banner mostrado correctamente desde GitHub."
